562a0c9dea5b58419c2b18547da422d7775d249b
JohanMenjivar <dbzbleach80@gmail.com>
2023-09-16 19:59:14 -0600

    primer avacnce, secciones 1 y 2

---
+++ sql's/Practicas select/Practica sql 12c select-part2.sql
@@ -0,0 +1,3 @@
+SELECT FIRST_NAME, SALARY AS "Bruto", (SALARY*.2) AS "Impuestos", (SALARY*.8) AS "Neto" FROM EMPLOYEES;
+
+SELECT FIRST_NAME AS "Nombre", SALARY AS "Salario", (SALARY * 14) AS "Salario anual" FROM EMPLOYEES;
